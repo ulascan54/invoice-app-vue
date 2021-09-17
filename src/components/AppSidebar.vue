16df485bf356f651441110f13efc9ded371349c7
ulascan54 <ulascandemirbag@gmail.com>
2021-09-18 01:41:23 +0300

    sidebardan ve dbden invoice silme butonu yapıldı

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -5,8 +5,8 @@
         <span>{{i+1}}</span>
         <span>{{invoice.contact.name}}</span>
         <span>
-          <button class="danger-button mr-1">
-            <svg xmlns="http://www.w3.org/2000/svg" class="fill-current" height="16" viewBox="0 0 24 24" width="16">
+          <button class="danger-button mr-1" @click="clearInvoice(invoice.id)">
+            <svg xmlns="http://www.w3.org/2000/svg"  class="fill-current" height="16" viewBox="0 0 24 24" width="16">
               <path d="M0 0h24v24H0V0z" fill="none" />
               <path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z" />
             </svg>
@@ -57,6 +57,16 @@ onMounted(()=>{
       });
       console.log(items.value);
 })
-  
+const clearInvoice = (id)=>{
+        db.collection("invoices")
+        .doc(id)
+        .delete()
+        .then(function () {
+          console.log("Document successfully deleted !");
+        })
+        .catch(function (error) {
+          console.error("Error adding document:", error);
+        });
+}
 
 </script>
