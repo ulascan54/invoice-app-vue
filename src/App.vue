b723e789cfc47577376d335cbbef4dec7dad8e80
ulascan54 <ulascandemirbag@gmail.com>
2021-09-17 19:46:26 +0300

    kaydet butonu ve görüntüle butonu yapıldı  derste olduğumuz konuma gelindi

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="w-screen h-screen bg-gray-800 flex flex-row text-white items-start justify-center">
-    <AppSidebar />
-    <InvoiceContent />
+    <AppSidebar :invoices="invoiceList" :invoiceDetails="invoiceDetails"/>
+    <InvoiceContent :saveInvoice="saveInvoice" :activeInvoice="state.activeInvoice"/>
   </div>
 </template>
 <script setup>
+import { reactive, ref } from '@vue/reactivity';
 import AppSidebar from './components/AppSidebar.vue';
 import InvoiceContent from './components/InvoiceContent.vue';
-
-
+const invoiceList=ref([])
+const state = reactive({activeInvoice:null})
+const saveInvoice =(invoice)=>{
+  invoiceList.value.push(invoice)
+  console.log('invoice :>> ', invoice);
+}
+const invoiceDetails =(invoice)=>{
+  state.activeInvoice=invoice;
+  setTimeout(() => {
+    state.activeInvoice=null
+  }, 1000);
+}
 </script>
--- src/components/InvoiceContent.vue
+++ src/components/InvoiceContent.vue
@@ -3,7 +3,6 @@
       <!-- FAtura Bilgileri -->
           <AppHeading title="FaturaBilgileri" />        
       <ContactSection :contact="state.contact" />
-          {{state.contact}}
       <div class="mt-5">
           <AppHeading title="Fatura Kalemleri"/>            
             <InvoiceItems :items="state.items" :AddInvoiceItem="AddInvoiceItem"/>
@@ -14,7 +13,7 @@
 
       <hr class="bg-gradient-to-r h-[1px] border-none from-gray-700 mt-5" />
       <div class="actionbar text-right my-5">
-        <button class="purple-button">Kaydet</button>
+        <button class="purple-button" @click="onSubmit">Kaydet</button>
       </div>
     </section>
 </template>
@@ -24,8 +23,9 @@ import ContactSection from './ContactSection.vue';
 import AppHeading from './ui/appHeading.vue';
 import InvoiceItems from './invoiceItems.vue';
 import InvoiceSummary from './invoiceSummary.vue';
-import { provide } from "@vue/runtime-core";
-import { reactive } from '@vue/reactivity';
+import { provide, watch } from "@vue/runtime-core";
+import { reactive} from '@vue/reactivity';
+const props = defineProps({saveInvoice:Function,activeInvoice:[Object,null]})
 const state = reactive({
   contact:{
     name:null,
@@ -51,4 +51,25 @@ const DeleteInvoiceItem = (invoiceItem)=>{
     state.items=state.items.filter(f=>f.id !== invoiceItem.id)
 }
 provide('DeleteInvoiceItem',DeleteInvoiceItem)
+const onSubmit = ()=>{
+  props.saveInvoice({...state, created_at: new Date().getTime(), id: new Date().getTime()})
+  state.contact= {
+    name:null,
+    email:null,
+    city:null,
+    country:null,
+    zipcode:null,
+  }
+  state.items=[]
+}
+  console.log(props.activeInvoice);
+
+watch(()=>props.activeInvoice,(activeInvoice)=>{
+      if (activeInvoice!==null) {
+        state.contact={
+          ...activeInvoice.contact
+        }
+        state.items=[...activeInvoice.items]
+      }
+})
 </script>
